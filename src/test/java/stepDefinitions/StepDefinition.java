1c34be0185a95adfa9b027f4e2279c34b4351db1
ElenaOstapchuk <elena.ostapchuk@gmail.com>
2017-08-03 21:43:34 +0200

    New step definitions

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -1,33 +1,34 @@
 package stepDefinitions;
 
+import static io.restassured.RestAssured.when;
 import static org.junit.Assert.assertEquals;
 
-import cucumber.api.java.en.Given;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.restassured.response.Response;
-import utils.Communicator;
+
+import java.util.Map;
 
 /**
  * Step definitions for tests
  */
 public class StepDefinition {
     private Response response;
-    private String securityToken = "";
-    private String singlePullRequestLink = "https://github.com/helenna1009/KeyPR_testing_task/pull/1";
 
-    @Given("user is logged in")
-    public void loggingIn(){
-        securityToken = Communicator.autorize();
-    }
-
-    @When("user retrieves single pull request")
-    public void retrievingSinglePullRequest(){
-        response = Communicator.makePullRequest(singlePullRequestLink, securityToken);
+    @When("user retrieves start page")
+    public void getLoginPage() {
+        response = when().get("http://github.com");
     }
 
     @Then("the status code is (\\d{3})")
-    public void verifyStatusCode(int statusCode){
+    public void verifyStatusCode(int statusCode) {
         assertEquals(statusCode, response.getStatusCode());
     }
+
+    @And("header contains (.*) with (.*)")
+    public void verifyParams(Map<String, String> paramsValues) {
+        assertEquals(paramsValues, response.getHeaders());
+    }
+
 }
